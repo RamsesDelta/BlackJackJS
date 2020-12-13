05911904e7ac28581aab383e9817292934c5f9b3
unknown <deltaghost.ram4@gmail.com>
2020-12-13 15:36:05 -0600

    new function

---
+++ assets/js/juego.js
@@ -0,0 +1,37 @@
+let deck = []
+const tipos = ['C', 'D', 'H', 'S']
+const especiales = ['A', 'J', 'Q', 'K']
+
+const crearDeck = () => {
+
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo)
+        }
+    }
+
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo)
+        }
+    }
+    deck = _.shuffle(deck)
+    console.log(deck)
+
+    return deck
+}
+
+const pedirCarta = () => {
+
+    if (deck.length === 0) {
+        throw 'No hay mas cartas'
+    }
+    const carta = deck.pop()
+    console.log(deck)
+    console.log(carta)
+    return carta;
+}
+
+
+crearDeck()
+
